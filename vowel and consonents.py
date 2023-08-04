afb0e838bab1767af71d1a7e8b844ca84648893f
sasivardhan1998 <sasivardhanmarolix.gmail.com>
2023-08-04 16:29:51 +0530

    initial commit

---
+++ vowel and consonents.py
@@ -0,0 +1,16 @@
+#ch=input("enter alphabet:")
+
+#if ch in ('a','e','i','o','u','A','E','I','O','U'):
+#    print(ch,"is vowel")
+#else:
+#    print(ch,"is consonent")
+
+
+ch=input("enter alphabet")
+if ch in ('a','e','i','o','u','A','E','I','O','U'):
+    print(ch,"is a vowel")
+else:
+    print(ch,"is consonents")
+
+
+
