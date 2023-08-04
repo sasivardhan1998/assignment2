10bf7988775a94f8aed6987f8fb1c7624e540c52
sasivardhan1998 <sasivardhanmarolix.gmail.com>
2023-08-04 15:57:41 +0530

    initial commit

---
+++ palindrome or not.py
@@ -0,0 +1,11 @@
+#str = input('Enter the string: ')
+#if str == str[::-1]:
+#   print('Palindrome')
+#else:
+#   print('Not')
+
+str=input("enter string:")
+if str == str[::-1]:
+   print("palindrome")
+else:
+   print("not")
