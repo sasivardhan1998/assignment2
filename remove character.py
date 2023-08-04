fb9d8dafcae708ec1f356f5629e13e7031fd7c81
sasivardhan1998 <sasivardhanmarolix.gmail.com>
2023-08-04 15:28:36 +0530

    initial commit

---
+++ remove character.py
@@ -0,0 +1,25 @@
+# using replace() method
+str = input("please enter a string : ")
+ch = input("please enter a character : ")
+print(str.replace(ch," ")) 
+
+# using replace() method
+str=input("enter string:")
+ch=input("enter character:")
+print(str.replace(ch," "))
+
+# using replace() method
+str=input("enter string")
+ch=input("enter character:")
+print(str.replace(ch," "))
+
+2 #Using translate() method
+def remove_char(s1,s2):
+    print(s1.replace(s2, ''))
+s1 = input("please give a String : ")
+s2 = input("please give a Character to remove : ")
+remove_char(s1,s2) 
+
+
+
+
